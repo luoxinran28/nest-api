f0064ba114369c6548725af7b74cea1686e52f1a
luoxinran28 <luoxinran28@gmail.com>
2022-11-15 23:18:09 +0800

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('Cats example')
