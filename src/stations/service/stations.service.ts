0987e17280430f97c80536c5e4067acaf90956ef
Xinran Luo <luoxinran28@gmail.com>
2022-12-08 14:06:09 +0800

    Add basic crud for stations service

--- src/stations/service/stations.service.ts
+++ src/stations/service/stations.service.ts
@@ -8,11 +8,24 @@ import { Station } from '../model/station.interface';
 export class StationsService {
   constructor(
     @InjectRepository(StationEntity)
-    private readonly stationsRepository: Repository<StationEntity>
+    private readonly stationRepository: Repository<StationEntity>
   ) {}
 
-  async createStation(station: Station, creator: User): Promise<Room> {
-    const newStation = await this.addCreatorToRoom(room, creator);
-    return this.roomRepository.save(newRoom);
+  async create(station: Station): Promise<Station> {
+    return this.stationRepository.save(station);
+  }
+
+  async findOne(id: number): Promise<Station> {
+    return this.stationRepository.findOne({ where: { id } });
+  }
+
+  async updateOne(id: number, station: Station): Promise<Station> {
+    this.stationRepository.update(id, station);
+    return await this.findOne(id);
+  }
+
+  async deleteOne(id: number): Promise<Station> {
+    this.stationRepository.delete(id);
+    return await this.findOne(id);
   }
 }
